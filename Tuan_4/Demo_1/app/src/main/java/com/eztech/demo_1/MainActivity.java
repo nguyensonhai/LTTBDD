cf840c41f0f0c9f81aeadd89bb323cc4063e3404
nguyensonhai <nguyensonhai1009@gmail.com>
2020-03-25 13:35:41 +0700

    update

---
+++ Tuan_4/Demo_1/app/src/main/java/com/eztech/demo_1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.eztech.demo_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnClick = findViewById(R.id.btnClick);
+
+    }
+}
